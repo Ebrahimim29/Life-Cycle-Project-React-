459cd5db616a804042008031a70232376a467a22
Ebrahimim29 <Ebrahimim29@gmail.com>
2025-11-02 07:17:43 +0330

    UseEffect

--- src/UserComponent.jsx
+++ src/UserComponent.jsx
@@ -1,9 +1,29 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 
 const UserComponent = () => {
 
     const [age, setAge] = useState(20);
+
+    //UseEffect: مدیریت چرخه حیات یک component
+    useEffect(() => {
+        //Mounting: زمانی که component بارگذاری می شود
+        console.log("send a request to the server");
+       
+        return () => {
+        //Unmounting: زمانی که component حذف می شود
+        console.log("Hoping to meet you");
+        };
+    }, []);
+    
+    useEffect(() => {
+        //Updating: زمانی که state تغییر می کند
+        // console.log(age);
+        if(age === 30){
+        console.log("Age is 30");
+        }
+    }, [age]);
+
     
     return (
         <div>
